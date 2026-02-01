a993e32cac16f649b6e2fcc83eaa696cb2b34977
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-02-02 00:37:28 +0530

    Day 9 - LeetCode 3827 Count Monobit Integers

---
+++ LeetCode/2026-02-01/3827_Count_Monobit_Integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countMonobit(int n) {
+        int count = 1;
+        int num = 1;
+        while(num <= n){
+            count++;
+            num = (num << 1) | 1;
+        }
+        return count;
+    }
+}
