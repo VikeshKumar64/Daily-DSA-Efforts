879dbd08d9625808c722f799748d459820fc607c
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-02-12 23:10:52 +0530

    Day 20 - LeetCode 344 Reverse String(Recursion)

---
+++ LeetCode/2026-02-12/344_Reverse_String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        helper(s,0,s.length - 1);
+    }
+    public void helper(char[] s, int l, int r){
+        if(l >= r) return;
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        helper(s,l+1,r-1);
+    }
+}
