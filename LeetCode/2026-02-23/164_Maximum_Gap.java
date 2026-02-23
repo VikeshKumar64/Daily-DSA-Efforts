4356b31f2b35a58e3d35e5256a77e3d278d8c113
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-02-23 22:33:06 +0530

    Day 31 - LeetCode 164 Maximum Gap

---
+++ LeetCode/2026-02-23/164_Maximum_Gap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length <= 1) return 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 1; i < nums.length; i++){
+            max = Math.max(max, (nums[i] - nums[i-1]));
+        }
+        return max;
+    }
+}
