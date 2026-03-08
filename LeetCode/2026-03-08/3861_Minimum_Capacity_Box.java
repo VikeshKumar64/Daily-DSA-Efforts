6c6fc720689fe0de312aa6322e649fc60a35f3a0
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-03-08 19:56:11 +0530

    Day 44 - LeetCode 3861 Minimum Capacity Box

---
+++ LeetCode/2026-03-08/3861_Minimum_Capacity_Box.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int ans = -1;
+        int curr = Integer.MAX_VALUE;
+        for(int i = 0; i < capacity.length; i++){
+            if(capacity[i] >= itemSize){
+                if(curr > capacity[i]){
+                    curr = capacity[i];
+                    ans = i;
+                }
+            }
+        }
+        return ans;
+    }
+}
