fb7c124323241b82ff7990104beec5739c0cb8e3
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-04-03 23:04:42 +0530

    Day 70 - LeetCode 739 Daily Temperatures

---
+++ LeetCode/2026-04-03/739_Daily_Temperatures.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        Stack<Integer> st = new Stack<>();
+        int ans[] = new int[len];
+
+        for(int i = len-1; i >= 0; i--){
+            while(!st.empty() && temperatures[st.peek()] <= temperatures[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = 0;
+            }else{
+                ans[i] = st.peek() - i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
