964104c169311dfa81f296a44ddfc969aa1f3ae1
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2026-09-20 23:12:39 +0530

    Day 240 - LeetCode 4056 Number of Intersecting Interval Pairs I

---
+++ LeetCode/2026-09-20/4056_Number_of_Intersecting_Interval_Pairs I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countIntersectingIntervals(int[][] intervals) {
+        int ans = 0;
+        for(int i = 0; i < intervals.length; i++){
+            for (int j = i + 1; j < intervals.length; j++) {
+
+                int start = Math.max(intervals[i][0], intervals[j][0]);
+                int end = Math.min(intervals[i][1], intervals[j][1]);
+
+                if (start <= end) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
